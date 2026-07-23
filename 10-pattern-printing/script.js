93459f3a4800b9d717c6c69a5bd382aadf8e6a90
Somil <somilraj55@gmail.com>
2026-07-23 09:40:05 +0530

    pattern printing task

--- 10-pattern-printing/script.js
+++ 10-pattern-printing/script.js
@@ -1,44 +1,3 @@
-// ===============================
-// Pattern 1
-// *
-// **
-// ***
-// ****
-// *****
-// ===============================
-
-for (let i = 1; i <= 5; i++) {
-
-    let star = "";
-
-    for (let j = 1; j <= i; j++) {
-        star += "*";
-    }
-
-    console.log(star);
-}
-
-// ===============================
-// Pattern 2
-// *****
-// ****
-// ***
-// **
-// *
-// ===============================
-
-for (let i = 5; i >= 1; i--) {
-
-    let star = "";
-
-    for (let j = i; j >= 1; j--) {
-        star += "*";
-    }
-
-    console.log(star);
-}
-
-
 // ========================================
 // Pattern 1 - Right Triangle
 // Output:
@@ -155,6 +114,82 @@ for (let i = 1; i <= 5; i++) {
 
     // Print Stars
     for (let j = 1; j <= 2 * i - 1; j++) {
+        star += "*";
+    }
+
+    console.log(star);
+}
+
+
+// ========================================
+// Pattern 6 - Inverted Pyramid Pattern
+// Output:
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+// ========================================
+
+for (let i = 1; i <= 5; i++) {
+    let star = "";
+
+    // Print Spaces
+    for (let j = 1; j <= i - 1; j++) {
+        star += " ";
+    }
+
+    // Print Stars
+    for (let j = 1; j <= 2 * (5 - i + 1) - 1; j++) {
+        star += "*";
+    }
+
+    console.log(star);
+}
+
+
+// ========================================
+// Pattern 7 - Diamond Pattern
+// Output:
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+// ========================================
+
+// Upper Pyramid
+for (let i = 1; i <= 5; i++) {
+    let star = "";
+
+    // Print Spaces
+    for (let j = 1; j <= 5 - i; j++) {
+        star += " ";
+    }
+
+    // Print Stars
+    for (let j = 1; j <= 2 * i - 1; j++) {
+        star += "*";
+    }
+
+    console.log(star);
+}
+
+// Lower Inverted Pyramid
+for (let i = 2; i <= 5; i++) {
+    let star = "";
+
+    // Print Spaces
+    for (let j = 1; j <= i - 1; j++) {
+        star += " ";
+    }
+
+    // Print Stars
+    for (let j = 1; j <= 2 * (5 - i) + 1; j++) {
         star += "*";
     }
 
